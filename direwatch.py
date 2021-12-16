04ad557c5465b7a62e894ad8fb9ad987bb5784b8
craigerl <craig@craiger.org>
2021-12-16 13:05:00 -0800

    Full featured full-screen display with comments, info, etc.  use -o optinon.

--- direwatch.py
+++ direwatch.py
@@ -3,7 +3,7 @@
 # direwatch
 
 """
-Craig Lamparter KM6LYW,  2021, MIT Licnese
+Craig Lamparter KM6LYW,  2022, MIT Licnese
 
 Code derived from Adafruit PIL python example
 
@@ -20,8 +20,7 @@ Do not install the kernel module/framebuffer.
 GPIO pins 12 (PTT) and 16 (DCD) are monitored and light green/red icons respectively.
 Configure these gpio pins in direwolf.
 
-
-Installation on raspbian/buster for short-attentions span programmers like me:
+Installation on raspbian/bullseye for short-attentions span programmers like me:
 
 sudo apt-get install python3-pip   # python >= 3.6 required
 sudo pip3 install adafruit-circuitpython-rgb-display
@@ -72,7 +71,6 @@ spi = board.SPI()
 #    board.SPI(),
 #    cs=cs_pin,
 #    dc=dc_pin,
-#    rst=reset_pin,
 #    baudrate=BAUDRATE,
 #    width=135,
 #    height=240,
@@ -86,11 +84,10 @@ disp = st7789.ST7789(
     spi,
     cs=cs_pin,
     dc=dc_pin,
-#    rst=reset_pin,
     baudrate=BAUDRATE,
     height=240,
     y_offset=80,
-    rotation=180
+    rotation=0
 )
 
 # don't write to display concurrently with thread
@@ -110,7 +107,6 @@ draw = ImageDraw.Draw(image)
 padding = 4 
 title_bar_height = 34
 
-
 def signal_handler(signal, frame):
    print("Got ", signal, " exiting.")
    draw.rectangle((0, 0, width, height), outline=0, fill=(30,30,30))
@@ -121,7 +117,6 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
-
 
 
 def parse_arguments():
@@ -151,32 +146,13 @@ else:
    title_text = "Direwatch"
 
 
-## GPIO buttons, pi tft uses logical 23 and 24 gpio pins
-## I moved this to direbuttons.py so we don't kill/restart ourselves
-## This is an example if you want to use the buttons here.
-## Uncomment the GPIO import at the top of this file if you want buttons.
-#GPIO.setwarnings(False)
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(23,GPIO.IN)
-#GPIO.setup(24,GPIO.IN)
-#
-#def button_callback(number):
-#    print("Button ",   number,  "  pressed.")
-#    if number == 23:
-#        pass
-#    if number == 24:
-#        pass
-#
-#GPIO.add_event_detect(23,GPIO.FALLING,callback=button_callback,bouncetime=300) 
-#GPIO.add_event_detect(24,GPIO.FALLING,callback=button_callback,bouncetime=300)
-       
-
-# Bluetooth LED connection check 
+# LED threads, bluetooth, RED, GREE"N
 
 def bluetooth_connection_poll_thread():
     bt_status = 0
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(5, GPIO.OUT) 
+    time.sleep(2) # so screen initialization doesn't overdraw bluetooth as off
 
     while True:
         cmd = "hcitool con | wc -l"
@@ -184,7 +160,6 @@ def bluetooth_connection_poll_thread():
         if int(connection_count) > 1:
             if bt_status == 0:
                 bt_status = 1
-                print("BT ON")
                 bticon = Image.open('bt.small.on.png')   
                 GPIO.output(5, GPIO.HIGH)
                 image.paste(bticon, (width - title_bar_height * 3 + 12  , padding + 2 ), bticon)
@@ -204,7 +179,21 @@ bluetooth_thread = threading.Thread(target=bluetooth_connection_poll_thread, nam
 bluetooth_thread.start()
 
 
-# Status LEDs thread
+def red_led_from_logfile_thread():                               ## RED logfile
+   #f = subprocess.Popen(['tail','-F',logfile], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+   f = subprocess.Popen(['tail','-F',logfile], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+   while True:
+      line = f.stdout.readline().decode("utf-8", errors="ignore")
+      search = re.search("^\[\d[A-Z]\]", line)
+      if search is not None:
+         draw.ellipse(( width - title_bar_height * 2           , padding,    width - title_bar_height - padding * 2 , title_bar_height - padding), fill=(200,0,0,0))
+         with display_lock:
+            disp.image(image)
+         time.sleep(1)
+         draw.ellipse(( width - title_bar_height * 2           , padding,    width - title_bar_height - padding * 2 , title_bar_height - padding), fill=(80,0,0,0))
+         with display_lock:
+            disp.image(image)
+
 
 def handle_changeG(cb):
    with open('/sys/class/gpio/gpio16/value', 'r') as f:          ## GREEN
@@ -218,7 +207,7 @@ def handle_changeG(cb):
    f.close
 
 def handle_changeR(cb):
-   with open('/sys/class/gpio/gpio12/value', 'r') as f:          ## RED
+   with open('/sys/class/gpio/gpio12/value', 'r') as f:          ## RED GPIO
       status = f.read(1)
       if status == '0':
          draw.ellipse(( width - title_bar_height * 2           , padding,    width - title_bar_height - padding * 2 , title_bar_height - padding), fill=(80,0,0,0))
@@ -240,16 +229,19 @@ wmR = pyinotify.WatchManager()
 notifierG = pyinotify.Notifier(wmG, default_proc_fun=null_function)
 notifierR = pyinotify.Notifier(wmR, default_proc_fun=null_function)
 
-# Watch both gpio pins for change
+# Watch both gpio pins for change if they exist
 wmG.add_watch('/sys/class/gpio/gpio16/value', pyinotify.IN_MODIFY)
-wmR.add_watch('/sys/class/gpio/gpio12/value', pyinotify.IN_MODIFY)
+if os.path.exists("/sys/class/gpio/gpio12/value/"):
+   wmR.add_watch('/sys/class/gpio/gpio12/value', pyinotify.IN_MODIFY)
 
-#Setup threads
 watch_threadG = threading.Thread(target=notifierG.loop, name="led-watcherG", kwargs=dict(callback=handle_changeG))
-watch_threadR = threading.Thread(target=notifierR.loop, name="led-watcherR", kwargs=dict(callback=handle_changeR))
-#Start threads after we display our logo, etc, much farther down in the code
-#watch_threadG.start()
-#watch_threadR.start()
+
+# Use gpio pin for red led if it exists, otherwise watch log file for transmit activity
+if os.path.exists("/sys/class/gpio/gpio12/value/"):
+   watch_threadR = threading.Thread(target=notifierR.loop, name="led-watcherR", kwargs=dict(callback=handle_changeR))
+else:
+   watch_threadR = threading.Thread(target=red_led_from_logfile_thread, name="redledthreadlog")
+
 
 # Load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
@@ -271,6 +263,7 @@ else:
    print("Couldn't find font " +  fontname_bold + " in working dir or /usr/share/fonts/truetype/dejavu/")
    exit(1)
 font = ImageFont.truetype(fontpath, fontsize)
+font_small = ImageFont.truetype(fontpath_bold, 18)
 font_big = ImageFont.truetype(fontpath_bold, 24)
 font_huge = ImageFont.truetype(fontpath_bold, 34)
 font_epic = ImageFont.truetype(fontpath, 40)
@@ -291,7 +284,7 @@ max_line_width = font.getsize("KN6MUC-15")[0] + symbol_dimension + (symbol_dimen
 max_cols = width // max_line_width
 
 # Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
+draw.rectangle((0, 0, width, height), outline=0, fill="#000000")
 
 # Draw our logo
 w,h = font.getsize(title_text)
@@ -301,7 +294,7 @@ with display_lock:
 time.sleep(1)
 
 # erase the screen
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
+draw.rectangle((0, 0, width, height), outline=0, fill="#000000")
 
 # draw the header bar
 draw.rectangle((0, 0, width, title_bar_height), fill=(30, 30, 30))
@@ -324,6 +317,7 @@ with display_lock:
 watch_threadG.start()
 watch_threadR.start()
 
+# setup screen geometries
 call = "null"
 x = padding
 max_lines  = ( height - title_bar_height - padding )  //   line_height 
@@ -332,34 +326,77 @@ line_count = 0
 col_count = 0 
 
 # tail and block on the log file
+<<<<<<< HEAD
+f = subprocess.Popen(['tail','-F','-n','10',logfile], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+=======
 f = subprocess.Popen(['tail','-F',logfile], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 #f = subprocess.Popen(['tail','-F','-n','80','/run/direwolf.log'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)  # debug 
+>>>>>>> 5cd9b2999d17c0c86901972ecfd79de7713009db
 
-##### one_loop() ############
-def one_loop():
+# Display loops.  list of stations, or a single station on the screen at a time
+
+def single_loop():
    symbol_chart0x64 = Image.open("aprs-symbols-64-0.png")
    symbol_chart1x64 = Image.open("aprs-symbols-64-1.png")
+
+   # we try to get callsign, symbol and four relevant info lines from every packet
    while True:
+      info1 = info2 = info3 = info4 = ''      # sane defaults
+
       line = f.stdout.readline().decode("utf-8", errors="ignore")
 
-      search = re.search("^\[\d\.\d\] (.*)", line)
+      search = re.search("^\[\d\.\d\] (.*)", line)                      # see if logfile line is an incoming packet over RF
       if search is not None:
          packetstring = search.group(1)
-         packetstring = packetstring.replace('<0x0d>','\x0d').replace('<0x1c>','\x1c').replace('<0x1e>','\x1e').replace('<0x1f>','\0x1f')
+         packetstring = packetstring.replace('<0x0d>','\x0d').replace('<0x1c>','\x1c').replace('<0x1e>','\x1e').replace('<0x1f>','\0x1f').replace('<0x0a>','\0x0a')
       else:
          continue
   
-      try:                                        #   aprslib has trouble parsing all packets
-         packet = aprslib.parse(packetstring)
+      try:  
+         packet = aprslib.parse(packetstring)                           # parse packet
+         print(packet)
          call = packet['from']
-         if 'symbol' in packet:
-            symbol = packet['symbol']
-            symbol_table = packet['symbol_table']
-         else:
-            symbol = '/'
+         supported_packet = True 
+      except Exception as e:                                            # aprslib doesn't support all packet types
+         print("Exception: aprslib: ", str(e), ": ", packetstring)
+         supported_packet = False
+         packet = {}   
+         search = re.search("^\[\d\.\d\] ([a-zA-Z0-9-]*)", line)        # snag callsign from unsupported packet
+         if search is not None:
+            call = search.group(1) 
+            symbol = '/'                                                # unsupported packet symbol set to red ball
             symbol_table = '/'
-      except:               #   aprslib has trouble parsing all packets
-         continue
+         else:
+            continue
+
+      try:          
+         if supported_packet:
+            if 'symbol' in packet:                                      # get symbol from valid packet or use red ball
+               symbol = packet['symbol']
+               symbol_table = packet['symbol_table']
+            else:
+               symbol = '/'
+               symbol_table = '/'
+                                                                        # extract relevant info lines
+         if not supported_packet:                                      
+               info1 = info2 = info3 = info4 = ''                       # no info in unsupported packet
+         elif 'weather' in packet:                                      # weather (often contained in compressed/uncompressed type packets)
+            info1 = round(packet['weather']['temperature'])
+            info1 = str(int(info1) * 1.8 + 32) + 'F'
+            print(info1)
+            info2 = str(packet['weather']['rain_since_midnight']) + '\" rain'
+            print(info2)
+            info3 = str(round(packet['weather']['wind_speed'])) + ' m/h'
+            info3 = info3 + ' ' + str(packet['weather']['wind_direction']) + '\''
+            print(info3)
+            info4 = str(packet['comment'])
+            print(info4)                                                # position packet
+         elif packet['format'] == 'mic-e' or packet['format'] == 'compressed'  or packet['format'] == 'uncompressed' or packet['format'] == 'object':  
+            info4 = packet['comment']                                   # fixme: comment is jibberish in all compressed packets                           
+         elif 'status' in packet:                                       # status packet
+            info4 = packet['status']
+      except Exception as e:
+         print("Malformed/missing data: ", str(e), ": ", packetstring)
 
       symbol_dimension = 64
       offset = ord(symbol) - 33
@@ -367,14 +404,19 @@ def one_loop():
       col = offset % 16 
       y = height // 3 
       x = width // 3  
-      draw.rectangle((0, title_bar_height, width, height), outline=0, fill=0)  # erase most of screen
+      draw.rectangle((0, title_bar_height, width, height), outline=0, fill="#000000")  # erase most of screen
       crop_area = (col*symbol_dimension, row*symbol_dimension, col*symbol_dimension+symbol_dimension, row*symbol_dimension+symbol_dimension)
       if symbol_table == '/':
          symbolimage = symbol_chart0x64.crop(crop_area)
       else:
          symbolimage = symbol_chart1x64.crop(crop_area)
       symbolimage = symbolimage.resize((height // 2, height // 2), Image.NEAREST)
-      image.paste(symbolimage, (0, 36), symbolimage)
+      #image.paste(symbolimage, (0, 36), symbolimage)
+      image.paste(symbolimage, (0, title_bar_height), symbolimage)
+      draw.text((120, 50), str(info1), font=font_small, fill="#AAAAAA")
+      draw.text((120, 70), str(info2), font=font_small, fill="#AAAAAA")
+      draw.text((120, 90), str(info3), font=font_small, fill="#AAAAAA")
+      draw.text((5, 144), str(info4), font=font_small, fill="#AAAAAA")
       draw.text((5, height - font_epic.getsize("X")[1] - 3), call, font=font_epic, fill="#AAAAAA") # text up from bottom edge
   
       with display_lock:
@@ -382,7 +424,7 @@ def one_loop():
       time.sleep(1)
 
 
-##### list_loop() ############
+# Display loops.  list of stations, or a single station on the screen at a time
 def list_loop():
   call = "null"
   # position cursor in -1 slot, as the first thing the loop does is increment slot
@@ -396,10 +438,11 @@ def list_loop():
   while True:
     line = f.stdout.readline().decode("utf-8", errors="ignore")
 
+    # watch for regular packet
     search = re.search("^\[\d\.\d\] (.*)", line)
     if search is not None:
        packetstring = search.group(1)
-       packetstring = packetstring.replace('<0x0d>','\x0d').replace('<0x1c>','\x1c').replace('<0x1e>','\x1e').replace('<0x1f>','\0x1f')
+       packetstring = packetstring.replace('<0x0d>','\x0d').replace('<0x1c>','\x1c').replace('<0x1e>','\x1e').replace('<0x1f>','\0x1f').replace('<0x0a>','\0x0a')
     else:
        continue
 
@@ -447,7 +490,7 @@ def list_loop():
        if col_count == max_cols:         # about to write off right edge of screen
            x = padding 
            y = padding + title_bar_height
-           draw.rectangle((0, title_bar_height + 1, width, height), outline=0, fill=0) # erase lines
+           draw.rectangle((0, title_bar_height + 1, width, height), outline=0, fill="#000000") # erase lines
            line_count = 0
            col_count = 0
            time.sleep(2.0)
@@ -463,7 +506,7 @@ def list_loop():
            disp.image(image)
 
 if args["one"]:
-   one_loop()
+   single_loop()
 else:
    list_loop()
 
